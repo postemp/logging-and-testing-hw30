9235915138f7df646790d983f8555dca33cc02ca
postemp <apostemp@mail.ru>
2023-10-16 00:34:44 +0300

    hw30

--- src/main/java/org/postemp/WorkingWithArrays.java
+++ src/main/java/org/postemp/WorkingWithArrays.java
@@ -6,28 +6,26 @@ public class WorkingWithArrays {
     public static void main(String[] args) {
         WorkingWithArrays workingWithArrays = new WorkingWithArrays();
 //        int[] inputArray = new int[]{1, 3, 2, 2};
-        int[] inputArray = new int[]{2, 2, 2};
+        int[] inputArray = new int[]{2, 1, 2, 2};
         System.out.println("входной массив состоит только из 1 и 2 : " + workingWithArrays.checkArray(inputArray));
         System.out.println(Arrays.toString(workingWithArrays.processArray(inputArray)));
     }
 
     public int[] processArray(int[] inputArray) {
         int foundIndex = -1;
+        int[] outArray;
         for (int i = inputArray.length - 1; i >= 0; i--) {
             if (inputArray[i] == 1) {
                 foundIndex = i;
+
                 break;
             }
         }
+
         if (foundIndex == -1) {
             throw new RuntimeException("Так и не встретили единичку :(");
         }
-        int[] outArray = new int[inputArray.length - 1 - foundIndex];
-        int cntr = 0;
-        for (int i = foundIndex + 1; i < inputArray.length; i++) {
-            outArray[cntr] = inputArray[i];
-            cntr++;
-        }
+        outArray = Arrays.copyOfRange(inputArray, foundIndex+1, inputArray.length );
         return outArray;
     }
 
