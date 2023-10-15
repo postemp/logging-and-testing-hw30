69d2553ee6b554e040b41ea4e3335b3f6f483980
postemp <apostemp@mail.ru>
2023-10-15 21:23:06 +0300

    hw30

--- src/main/java/org/postemp/WorkingWithArrays.java
+++ src/main/java/org/postemp/WorkingWithArrays.java
@@ -5,20 +5,15 @@ import java.util.Arrays;
 public class WorkingWithArrays {
     public static void main(String[] args) {
         WorkingWithArrays workingWithArrays = new WorkingWithArrays();
-//        int[] inputArray = new int[]{1, 2, 2, 2, 2};
-//        int[] inputArray = new int[]{1, 2, 1, 2, 1};
-//        int[] inputArray = new int[]{1, 2, 1, 1, 2};
-//        int[] inputArray = new int[]{1, 2, 1, 2, 2};
 //        int[] inputArray = new int[]{1, 3, 2, 2};
         int[] inputArray = new int[]{2, 2, 2};
-//        int[] inputArray = new int[0];
-//        System.out.println("входной массив состоит только из 1 и 2 : "+ workingWithArrays.checkArray(inputArray));
+        System.out.println("входной массив состоит только из 1 и 2 : " + workingWithArrays.checkArray(inputArray));
         System.out.println(Arrays.toString(workingWithArrays.processArray(inputArray)));
     }
 
-    public  int[] processArray(int[] inputArray) {
+    public int[] processArray(int[] inputArray) {
         int foundIndex = -1;
-        for (int i = inputArray.length-1; i >= 0; i--) {
+        for (int i = inputArray.length - 1; i >= 0; i--) {
             if (inputArray[i] == 1) {
                 foundIndex = i;
                 break;
@@ -27,15 +22,16 @@ public class WorkingWithArrays {
         if (foundIndex == -1) {
             throw new RuntimeException("Так и не встретили единичку :(");
         }
-        int[] outArray = new int[inputArray.length-1 - foundIndex];
+        int[] outArray = new int[inputArray.length - 1 - foundIndex];
         int cntr = 0;
-        for (int i = foundIndex+1; i < inputArray.length; i++) {
+        for (int i = foundIndex + 1; i < inputArray.length; i++) {
             outArray[cntr] = inputArray[i];
             cntr++;
         }
         return outArray;
     }
-    public  boolean checkArray(int[] inputArray) {
+
+    public boolean checkArray(int[] inputArray) {
         boolean itHas1 = false;
         boolean itHas2 = false;
         for (int i = 0; i < inputArray.length; i++) {
--- src/test/java/org/postemp/tests/ProcessArrayTests.java
+++ src/test/java/org/postemp/tests/ProcessArrayTests.java
@@ -1,4 +1,4 @@
-package org.postemp.calculator.tests;
+package org.postemp.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
